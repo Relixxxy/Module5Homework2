1d0c1ea66f4f9f71df4ee329787c9497c48b075e
Relixxxy <vut1111111@gmail.com>
2023-01-27 23:30:36 +0200

    catalog api, nginx and docker compose

---
+++ Catalog.Host/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.Host.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Host.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IList<Product> _list;
+        private readonly int _size = 10;
+
+        public ProductsController()
+        {
+            var rand = new Random(DateTime.Now.Millisecond);
+            _list = new List<Product>();
+
+            for (int i = 0; i < _size; i++)
+            {
+                _list.Add(new Product(i, $"Product_{i}", $"Catalog_{rand.Next(33, 77)}"));
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_list);
+        }
+    }
+}
---
+++ Catalog.Host/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Catalog.Host.Models
+{
+    public class Product
+    {
+        public Product(int id, string name, string category)
+        {
+            Id = id;
+            Name = name;
+            Category = category;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Category { get; set; } = null!;
+    }
+}
